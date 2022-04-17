1e84e17f5a2e755f1d3b3ac305cbac54bf48a484
Baisakhi Das <60517623+baisakhidas11@users.noreply.github.com>
2022-04-17 20:11:50 +0530

    Create Chart.js

---
+++ Chart.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ChartBar from './ChartBar.js';
+//import './ChartBar.css';
+import './Chart.css';
+
+const Chart = props => {
+const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    //const dataPointValues = props.datapoints.map(dataPoint => dataPoint.value);
+const totalMaximum = Math.max(...dataPointValues);
+
+    return (
+    <div className="chart">
+    {props.dataPoints.map((dataPoint)=>(
+         <ChartBar 
+         key={dataPoint.label}
+         value= {dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+          /> 
+         ) )}
+    </div>
+    );
+};
+
+export default Chart;
